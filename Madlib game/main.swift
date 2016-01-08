92974fa24bb15b0cb276b9105409053d26e41be4
Guest User <coder@mainlab11.local>
2016-01-08 10:40:12 -0800

    heuheuhuehuee

--- Madlib game/main.swift
+++ Madlib game/main.swift
@@ -12,4 +12,37 @@ import Foundation
 print("Hello, World!")
 print("Hello, World!")
 
-//fhuaefhliudsaolgd
+
+import Foundation
+
+print("test")
+
+print("test")
+
+func input() -> String {
+    var keyboard = NSFileHandle.fileHandleWithStandardInput()
+    var inputData = keyboard.availableData
+    var strData = NSString(data: inputData, encoding: NSUTF8StringEncoding)!
+    
+    return strData.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+}
+import Foundation
+
+print("Name of Company")
+var a = input()
+
+print("What are you inventing?")
+var b = input()
+
+print("Who?")
+var c = input()
+
+print("Solve a Problem")
+var d = input()
+
+print("Secret Sauce")
+var e = input()
+
+
+print("My company \(a) is developing \(b) to help \(c) \( d) with \(e)")
+
